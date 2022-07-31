38b153a44c9036826999d9bb4fef9fb0e6bd2365
IBM_ESP <boigues50@gmail.com>
2022-07-31 19:25:40 +0200

    Non Players Skull and anottaions

---
+++ src/main/java/com/gmail/ibmesp1/ibcore/skull/NonPlayerSkulls.java
@@ -0,0 +1,40 @@
+package com.gmail.ibmesp1.ibcore.skull;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+public class NonPlayerSkulls {
+
+    private String key;
+
+    public NonPlayerSkulls() {
+        key = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUv";
+    }
+
+    public ItemStack getSkull(String url) {
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        if ((key + url).isEmpty()) {
+            return head;
+        }
+
+        SkullMeta headMeta = (SkullMeta) head.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+        profile.getProperties().put("textures", new Property("textures", key + url));
+        Field profileField;
+        try {
+            profileField = headMeta.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(headMeta, profile);
+        } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException ignored) {
+
+        }
+        head.setItemMeta(headMeta);
+        return head;
+    }
+}
--- src/main/java/com/gmail/ibmesp1/ibcore/utils/Utils.java
+++ src/main/java/com/gmail/ibmesp1/ibcore/utils/Utils.java
@@ -4,10 +4,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 public class Utils {
+
+    /**
+     * Displays an urgent message to the console server
+     *
+     * @param name Plugin name
+     * @param msg Message you want to display
+     */
     public static void urgentConsoleWarning(String name,String msg) {
         Bukkit.getConsoleSender().sendMessage(name + " " + ChatColor.RED + msg);
     }
 
+    /**
+     * Capitalizes the first letter of a string
+     *
+     * @param string String you want to capitalize
+     * @return String with the first letter capitalized
+     */
     public static String capitalizeFirstLetter(String string) {
         return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
