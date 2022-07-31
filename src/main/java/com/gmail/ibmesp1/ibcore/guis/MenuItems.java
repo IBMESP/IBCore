ce634bd45775b6dde2a6178c078f224c3d97c658
IBM_ESP <boigues50@gmail.com>
2022-07-31 13:41:20 +0200

    Menu items

--- src/main/java/com/gmail/ibmesp1/ibcore/guis/MenuItems.java
+++ src/main/java/com/gmail/ibmesp1/ibcore/guis/MenuItems.java
@@ -4,13 +4,26 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.List;
+
 public class MenuItems {
 
-    private ItemStack item;
+    public ItemStack glass(){
+        ItemStack glass = new ItemStack(Material.BLACK_STAINED_GLASS);
+        ItemMeta meta = glass.getItemMeta();
+        meta.setDisplayName(" ");
+        glass.setItemMeta(meta);
+        return glass;
+    }
 
-    public ItemStack createItem(Material material,String name){
+    public ItemStack createItem(Material material, String name, List<String> lore){
+        ItemStack item = new ItemStack(material);
         ItemMeta meta = item.getItemMeta();
         meta.setDisplayName(name);
+
+        if(lore != null)
+            meta.setLore(lore);
+
         item.setItemMeta(meta);
         return item;
     }
