a0bec642fe9416295738ab35f04f0e0c24bc09ee
IBM_ESP <boigues50@gmail.com>
2022-08-07 22:13:12 +0200

    PlayerSkull

--- src/main/java/com/gmail/ibmesp1/ibcore/commands/SubCommand.java
+++ src/main/java/com/gmail/ibmesp1/ibcore/commands/SubCommand.java
@@ -5,9 +5,7 @@ import org.bukkit.command.CommandSender;
 import java.util.List;
 
 public abstract class SubCommand {
-
     public abstract String getName();
     public abstract void perform(CommandSender sender, String[] args);
-
     public abstract List<String> getSubCommandsArgs(String[] args);
 }
