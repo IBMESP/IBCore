ef0faa9c98d0ea5409670cfe17394ac54c9f9ad8
IBM_ESP <boigues50@gmail.com>
2022-08-09 00:22:25 +0200

    1.0.3

--- src/main/java/com/gmail/ibmesp1/ibcore/skull/Skulls.java
+++ src/main/java/com/gmail/ibmesp1/ibcore/skull/Skulls.java
@@ -2,18 +2,20 @@ package com.gmail.ibmesp1.ibcore.skull;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import java.lang.reflect.Field;
 import java.util.UUID;
 
-public class NonPlayerSkulls {
+public class Skulls {
 
     private String key;
 
-    public NonPlayerSkulls() {
+    public Skulls() {
         key = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUv";
     }
 
@@ -44,5 +46,21 @@ public class NonPlayerSkulls {
         }
         head.setItemMeta(headMeta);
         return head;
+    }
+
+    /**
+     * Creates a skull with a player skin
+     *
+     * @param player The player skin you want to use
+     * @return An ItemStack skull with a texture
+     *
+     */
+    public ItemStack playerSkull(Player player){
+        ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+        skullMeta.setOwningPlayer(player);
+        skullMeta.setDisplayName(ChatColor.GOLD + player.getName());
+        skull.setItemMeta(skullMeta);
+        return skull;
     }
 }
